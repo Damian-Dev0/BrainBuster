169b5999ba5c3dc68b63851a293211275e10591b
CentaKlaus <2000pst2000@gmail.com>
2024-03-07 11:30:07 +0100

    Main View

--- brainbuster/settings.py
+++ brainbuster/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ahy2z_akc-wnvmhmhh9(uq&f@^%brc5kea5@uhlmvaik+2d+(a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'quiz'
 ]
 
 MIDDLEWARE = [
