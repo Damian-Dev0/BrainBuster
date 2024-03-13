cb58019d3ac6e04d4fff28de80a32281e55e4731
CentaKlaus <2000pst2000@gmail.com>
2024-03-13 15:24:05 +0100

    html + logos

--- BrainBusterSite/BrainBusterSite/urls.py
+++ BrainBusterSite/BrainBusterSite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from BrainBusterApp.views import menu
 
 urlpatterns = [
     path('BrainBusterApp/', include('BrainBusterApp.urls')),
+    path('menu/', menu),
     path('admin/', admin.site.urls),
 ]
