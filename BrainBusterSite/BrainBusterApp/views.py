d6c22af388a2aef91a26290d38754a26bf0e3edf
CentaKlaus <2000pst2000@gmail.com>
2024-03-16 09:34:35 +0100

    Zufallsfrage auswählen + Antworten

--- BrainBusterSite/BrainBusterApp/views.py
+++ BrainBusterSite/BrainBusterApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Question
+from .models import Question, Answers
+
+import random
 
 # Create your views here.
 def index(request):
@@ -11,7 +13,9 @@ def menu(request):
 
 def question(request):
     fragen = Question.objects.all()
-    return render(request, 'question.html', {'fragen': fragen})
+    zufall_frage = random.choice(fragen)
+    antworten = zufall_frage.answers_set.all()
+    return render(request, 'question.html', {'frage': zufall_frage, 'antworten': antworten})
 
 def login(request):
     return render(request, 'login.html')
